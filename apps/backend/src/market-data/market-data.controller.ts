06f6be78d4d02f6826d70bc1d6bef1111a11b528
Rodrigo Moguillansky <romoguill@gmail.com>
2025-11-21 11:18:43 -0300

    Exposed market service

--- apps/backend/src/market-data/market-data.controller.ts
+++ apps/backend/src/market-data/market-data.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
 import { ThirdPartyApiService } from "@repo/third-party-api";
-import { AllowAnonymous } from "@thallesp/nestjs-better-auth";
 
 @Controller("market-data")
 export class MarketDataController {
   constructor(private readonly marketDataService: ThirdPartyApiService) {}
 
   @Get()
-  @AllowAnonymous()
   getMarketData() {
-    console.log(this.marketDataService.getCurrentMarketPrice("CORN"));
     return this.marketDataService.getCurrentMarketPrice("CORN");
   }
 }
--- packages/third-party-api/src/third-party-api.service.ts
+++ packages/third-party-api/src/third-party-api.service.ts
@@ -1,6 +1,24 @@
-import { Injectable, Logger } from "@nestjs/common";
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from "@nestjs/common";
 import { CROPS_API } from "./types/types";
 import { ConfigService } from "@nestjs/config";
+
+type MatbaCedentials = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+type MatbaMarketData = {
+  indexValue: number;
+  maturity: number;
+  mdEntryDateTime: string;
+  unixTimestamp: number;
+  name: string;
+};
 
 @Injectable()
 export class ThirdPartyApiService {
@@ -26,28 +44,62 @@ export class ThirdPartyApiService {
       this.configService.getOrThrow("MATBA_API_PASSWORD");
   }
 
-  async getToken(): Promise<{ access: string; refresh: string }> {
+  private async getToken(
+    credentials: MatbaCedentials
+  ): Promise<{ access: string; refresh: string }> {
     const response = await fetch(this.MATBA_API_LOGIN_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        name: this.MATBA_API_USERNAME,
-        email: this.MATBA_API_EMAIL,
-        password: this.MATBA_API_PASSWORD,
-      }),
+      body: JSON.stringify({ ...credentials }),
     });
 
+    const body = await response.json();
+
     if (!response.ok) {
+      this.logger.error(JSON.stringify(body));
       throw new Error("Failed to get token");
     }
 
-    const data = await response.json();
-    return data;
+    return body;
   }
 
-  getCurrentMarketPrice(symbol: keyof typeof CROPS_API) {
-    return { symbol, price: 100 };
+  private async getData(
+    symbol: keyof typeof CROPS_API,
+    token: string
+  ): Promise<MatbaMarketData> {
+    const response = await fetch(`${this.MATBA_API_MARKET_URL}${symbol}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const body = await response.json();
+
+    if (!response.ok) {
+      this.logger.error(JSON.stringify(body));
+      throw new InternalServerErrorException("Failed to get market data");
+    }
+
+    return body;
+  }
+
+  async getCurrentMarketPrice(symbol: keyof typeof CROPS_API) {
+    try {
+      const token = await this.getToken({
+        username: this.MATBA_API_USERNAME,
+        email: this.MATBA_API_EMAIL,
+        password: this.MATBA_API_PASSWORD,
+      });
+
+      const data = await this.getData(symbol, token.access);
+      return data;
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException("Failed to get market data");
+    }
   }
 }
